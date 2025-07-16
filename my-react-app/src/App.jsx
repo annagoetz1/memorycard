26f0ae3f4927e6727cb99c9d9f15385f5f359657
annagoetz1 <annalgoetz1@gmail.com>
2025-07-16 17:27:16 -0400

    Add score <p> into parent

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,8 @@
 
-
+import OfficeGifs from './OfficeGifs';
 import './App.css'
+
+import handleClick from './OfficeGifs';
 
 // App.jsx or a component file
 import { useEffect, useState } from 'react';
@@ -8,17 +10,12 @@ import { useEffect, useState } from 'react';
 function App() {
   return (
 
-  <div style={{ padding: '2rem' }}>
-    <h1>My Favorite GIPHY</h1>
-
-    {/* GIPHY embed as an image */}
-    <img
-  src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExODZubW1jbG1hbnN6MWs1YzlmcG9ldGk5aGUyM2h5dnlsZnduenp0eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/cXblnKXr2BQOaYnTni/giphy.gif"
-  alt="Michael Scott"
-  width={400}
-/>
+  <div>
+    <OfficeGifs/>
 
   </div>
+  
+
 );
 }
 export default App;
